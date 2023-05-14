57c64d1d0561b8d2ce0c1b3b0b67407508d6450e
Shaxzod <shaxzodbekbozorov@gmail.com>
2023-05-14 12:22:42 +0500

    refactor javascript

--- hospital/static/js/scripts.js
+++ hospital/static/js/scripts.js
@@ -1,21 +1,41 @@
+async function postData(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/x-www-form-urlencoded',
+            'X-CSRFToken': csrf_token,
+        },
+        body: new URLSearchParams(data),
+    });
+    console.log(new URLSearchParams(data));
+
+    if (!response.ok) {
+        throw new Error(`Network response was not OK: ${response.statusText}`);
+    }
+
+    return response;
+}
+
+async function removeElements(user_id) {
+    const button = document.getElementById('list-' + user_id);
+    const listItem = document.getElementById('list-' + user_id + '-list');
+    button.remove();
+    listItem.remove();
+}
 
 
 async function sendAjaxData(user_id, action) {
-    const formData = new FormData();
-    formData.set('user_id', user_id);
-    formData.set('action', action);
-    formData.set("csrfmiddlewaretoken", csrf_token);
-    console.log(formData);
-    const response = await fetch(patient_review_url, {
-        method: 'POST',
-        body: formData
-    });
-    if (response.ok) {
-        let button = document.getElementById('list-' + user_id);
-        let listItem = document.getElementById('list-'+user_id+'-list');
-        button.remove();
-        listItem.remove();
+    try{
+        const data = {
+            user_id: user_id,
+            action: action,
+        };
+        const response = await postData(patient_review_url, data);
+        if (response.ok){
+            await removeElements(user_id);
+        }
+    } catch (error) {
+        console.log(error);
     }
-
 
 }
